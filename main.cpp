319fa32573935eacd8e5199cdf247270412cae57
Kamir Walton <kamirwalton13@gmail.com>
2023-03-30 19:52:39 -0400

    All the pre DDA algorithm calculations are done.

--- main.cpp
+++ main.cpp
@@ -71,6 +71,55 @@ int main(int /*argc*/, char*/*argv*/[])
 			double cameraX = ((2 * x) / double(mapWidth)) - 1; // x-coord in camera space
 			double rayDirX = dirX + planeX * cameraX;
 			double rayDirY = dirY + planeY * cameraX;
+
+			// Determines which box of the map we are in
+			int mapX = int(posX);
+			int mapY = int(posY);
+
+			// Length of ray from current position
+			double sideDistX;
+			double sideDistY;
+
+			// Length of ray from one x-side or y-side to the next x-side or y-side
+			double deltaDistX = std::abs(1 / rayDirX);
+			double deltaDistY = std::abs(1 / rayDirY);
+			double perpWallDist;
+
+			// What direction to step in (x or y [going to be +1 or -1])
+			int stepX;
+			int stepY;
+
+			// Determines whether or not a wall was hit
+			int hit = 0;
+
+			// Determines what side was hit (x or y [0 for x-side, 1 for y-side])
+			int side;
+
+			// Calculate initial step and sideDist (having some trouble understanding the calculations on this part...)
+			if (rayDirX < 0)
+			{
+				stepX = -1;
+				sideDistX = (posX - mapX) * deltaDistX;
+			}
+			else
+			{
+				stepX = 1;
+				sideDistX = ((mapX + 1.0) - posX) * deltaDistX;
+			}
+
+			if (rayDirY < 0)
+			{
+				stepY = -1;
+				sideDistY = (posY - mapY) * deltaDistY;
+			}
+			else
+			{
+				stepY = 1;
+				sideDistY = ((mapY + 1.0) - posY) * deltaDistY;
+			}
+
+			// ********** THIS IS WHERE THE DDA ALGORITHM ACTUALLY STARTS **********
+
 		}
 	}
 
